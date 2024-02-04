ddf11fe61a04c59fdf60de209260dc01d935f40c
julian arias <tattoojulian@myhost.local>
2024-02-04 01:53:58 +0100

    procfile

--- app/openai_client.py
+++ app/openai_client.py
@@ -1,6 +1,31 @@
 import os
 import openai
 
+
+class OpenAIClient:
+    def __init__(self):
+        print("\nopenai key is" + client.api_key )
+
+    def complete(self, prompt):
+        response = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt,
+                }
+            ],
+            model="gpt-3.5-turbo",
+            
+        prompt=prompt,
+        temperature=0.0,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+        )
+
+        print("response form openai is :\n" + str(response) + "\n")
+        return response.choices[0].message.content
 class OpenAIClient:
     def __init__(self):
         openai.api_key = os.environ.get("OPENAI_API_KEY")
