9650e8038717ca348fe132121410ef807ce4d12c
tattojulian <158783078+tattojulian@users.noreply.github.com>
2024-02-06 22:55:46 +0100

    Update openai_client.py

--- app/openai_client.py
+++ app/openai_client.py
@@ -11,14 +11,17 @@ class OpenAIClient:
     def __init__(self):
         print("\nopenai key is" + oclient.api_key)
 
-    def complete(self, prompt, phone_number):
+        def complete(self, prompt, phone_number):
         # Retrieve the last 10 interactions from the database for the given phone number.
-        all_conversations = db.read_list_record("conversations", phone_number, default=[])
-        last_5_conversations = all_conversations[-10:]
+        previous_conversation=None
+        try:
+            all_conversations = db.read_list_record("conversations", phone_number, default=[])
+            last_5_conversations = all_conversations[-10:]
         # Format the previous conversations to provide context to the GPT model.
-        previous_conversation = "\n".join(
+            previous_conversation = "\n".join(
             [f'User: {conv["user_message"]}\nAssistant: {conv["gpt_response"]}' for conv in last_5_conversations])
-
+        except:
+            pass
         # System message providing guidelines and context to the GPT model.
         system_message = """Eres un asistente virtual de un estudio de tatuajes en la ciudad de Zaragoza españa. se llama tattoojulian studio, en dicho studio tatuan Julian, cuyo estilo es realista, chechu que su estilo es old school, tu vas a atender los mensajes que recibamos desde whatsapp, te agregare contexto de acuerdo a las conversaciones que hayas tenido con el usuario. Tus objetivos como agente, son que el usuario aparte una cita con uno de nuestros tatuadores, y, que cuando detectes que el usuario quiere una cita, retornes al final del texto de tu respuesta {%QUIERE_CITA%}. debes obtener el nombre del tatuador, cuando sea asi escribir al final el texto {%TATUADOR <nombre_tatuador>%}, que se quiere tatuar, en que parte del cuerpo{%ZONA <zona_cuerpo> %} y concretar una cita{%CITA <fecha> <hora> %}. inmediatamente tengas esos datos, escribes como la plantilla indica, al final de tu respuesta"""
 
